2f0c2867eaa7573fbc5a798e915bbaff34435ada
Jesse <jessepena@hotmail.es>
2019-03-08 23:14:19 -0400

    Uploaded dashboard code

--- app/src/main/java/com/example/turnosandroid_pucmm/Jesse/DashboardActivity.java
+++ app/src/main/java/com/example/turnosandroid_pucmm/Jesse/DashboardActivity.java
@@ -1,15 +1,56 @@
 package com.example.turnosandroid_pucmm.Jesse;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Toast;
 
-import com.example.turnosandroid_pucmm.R;
+import java.util.ArrayList;
 
-public class DashboardActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> listDatos;
+    RecyclerView recycler;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_main);
+
+        toolbar=findViewById(R.id.idToolbar);
+        setSupportActionBar(toolbar);
+
+        recycler=findViewById(R.id.recyclerId);
+        recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        listDatos= new ArrayList<String>();
+
+        for(int i=0; i<5; i++) {
+            listDatos.add("Sucursal #" + i + " ");
+        }
+        AdapterDatos adapter = new AdapterDatos(listDatos);
+
+        adapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Selección: " + listDatos.get
+                                (recycler.getChildAdapterPosition(v)), Toast.LENGTH_SHORT).show();
+            }
+        });
+        recycler.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
